719c292e6073d6c72840d57fda566a8361370592
estradax <azharumuhammad@gmail.com>
2024-02-23 01:07:33 +0700

    ajskdjaskdt

--- resources/views/partials/header-brand.blade.php
+++ resources/views/partials/header-brand.blade.php
@@ -128,11 +128,8 @@
                 </div>
               </a>
               <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                <a href="#" class="dropdown-item">Status</a>
                 <a href="{{ route('profile.index') }}" class="dropdown-item">Profile</a>
-                <a href="#" class="dropdown-item">Feedback</a>
                 <div class="dropdown-divider"></div>
-                <a href="./settings.html" class="dropdown-item">Settings</a>
                 <form action="{{ route('logout.store') }}" method="post">
                     @csrf
                     <button class="dropdown-item">Logout</a>
