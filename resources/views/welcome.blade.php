91b63defb294a2c63c5566956743a468ec0bbb04
estradax <azharumuhammad@gmail.com>
2024-02-23 20:47:55 +0700

    feat: remove unsajdksajdkasjdkakdjskajdksa ivan anjing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -92,20 +92,6 @@
                       </div>
                     </div>
                     <div class="h1 mb-3">75%</div>
-                    <div class="d-flex mb-2">
-                      <div>Persentase</div>
-                      <div class="ms-auto">
-                        <span class="text-green d-inline-flex align-items-center lh-1">
-                          7% <!-- Download SVG icon from http://tabler-icons.io/i/trending-up -->
-                          <svg xmlns="http://www.w3.org/2000/svg" class="icon ms-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 17l6 -6l4 4l8 -8" /><path d="M14 7l7 0l0 7" /></svg>
-                        </span>
-                      </div>
-                    </div>
-                    <div class="progress progress-sm">
-                      <div class="progress-bar bg-primary" style="width: 75%" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" aria-label="75% Complete">
-                        <span class="visually-hidden">75% Complete</span>
-                      </div>
-                    </div>
                   </div>
                 </div>
               </div>
@@ -126,15 +112,8 @@
                     </div>
                     <div class="d-flex align-items-baseline">
                       <div class="h1 mb-0 me-2">80,45</div>
-                      <div class="me-auto">
-                        <span class="text-green d-inline-flex align-items-center lh-1">
-                          8% <!-- Download SVG icon from http://tabler-icons.io/i/trending-up -->
-                          <svg xmlns="http://www.w3.org/2000/svg" class="icon ms-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 17l6 -6l4 4l8 -8" /><path d="M14 7l7 0l0 7" /></svg>
-                        </span>
-                      </div>
                     </div>
                   </div>
-                  <div id="chart-revenue-bg" class="chart-sm"></div>
                 </div>
               </div>
               <div class="col-sm-6 col-lg-3">
@@ -155,13 +134,7 @@
                     </div>
                     <div class="d-flex align-items-baseline">
                       <div class="h1 mb-3 me-2">81,00</div>
-                      <div class="me-auto">
-                        <span class="text-green d-inline-flex align-items-center lh-1">
-                          5% <!-- Download SVG icon from http://tabler-icons.io/i/minus -->
-                          <svg xmlns="http://www.w3.org/2000/svg" class="icon ms-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 17l6 -6l4 4l8 -8" /><path d="M14 7l7 0l0 7" /></svg>
-                      </div>
                     </div>
-                    <div id="chart-new-clients" class="chart-sm"></div>
                   </div>
                 </div>
               </div>
@@ -183,14 +156,7 @@
                     </div>
                     <div class="d-flex align-items-baseline">
                       <div class="h1 mb-3 me-2">85%</div>
-                      <div class="me-auto">
-                        <span class="text-green d-inline-flex align-items-center lh-1">
-                          4% <!-- Download SVG icon from http://tabler-icons.io/i/trending-up -->
-                          <svg xmlns="http://www.w3.org/2000/svg" class="icon ms-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 17l6 -6l4 4l8 -8" /><path d="M14 7l7 0l0 7" /></svg>
-                        </span>
-                      </div>
                     </div>
-                    <div id="chart-active-users" class="chart-sm"></div>
                   </div>
                 </div>
               </div>
