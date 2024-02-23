b8d48046bcc22dfd3ab59dbe265a4074b6fb8d4a
estradax <azharumuhammad@gmail.com>
2024-02-23 21:01:51 +0700

    feat: nanda ganteng banget man kitu

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -232,11 +232,6 @@
                           <svg xmlns="http://www.w3.org/2000/svg" class="icon me-2 text-muted" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12z" /><path d="M16 3v4" /><path d="M8 3v4" /><path d="M4 11h16" /><path d="M11 15h1" /><path d="M12 15v3" /></svg>
                           Birth date: <strong>7/8/2003</strong>
                         </div>
-                        <div>
-                          <!-- Download SVG icon from http://tabler-icons.io/i/clock -->
-                          <svg xmlns="http://www.w3.org/2000/svg" class="icon me-2 text-muted" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M12 7v5l3 3" /></svg>
-                          Time zone: <strong>Indonesia WIB</strong>
-                        </div>
                       </div>
                     </div>
                   </div>
